3a3e90f10d9a8e405770f129b4b70565b2695523
misosviso <michal.komora@protonmail.com>
2024-02-22 19:07:44 +0100

    first commit

---
+++ src/test/java/sk/stu/fiit/aass/centralreservationsystem/ApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.stu.fiit.aass.centralreservationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
