06cc13ce1f3db2dd770b8430fd579e6bf711312a
misosviso <michal.komora@protonmail.com>
2024-03-28 13:32:19 +0100

    MICROSERVCIES!

---
+++ gateway-server/src/main/java/sk/stu/fiit/gateway/filter/RequestFilter.java
@@ -0,0 +1,25 @@
+package sk.stu.fiit.gateway.filter;
+
+import sk.stu.fiit.gateway.model.Company;
+import sk.stu.fiit.gateway.model.Student;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class RequestFilter implements GatewayFilter {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        Object body = exchange.getAttribute("cachedRequestBodyObject");
+        System.out.println("in request filter");
+        if(body instanceof Student) {
+            System.out.println("body:" + (Student) body);
+        }
+        else if(body instanceof Company) {
+            System.out.println("body:" + (Company) body);
+        }
+        return chain.filter(exchange);
+    }
+}
---
+++ reservation-service/src/main/java/com/example/firstmicroservice/controller/ReservationController.java
@@ -0,0 +1,26 @@
+package com.example.firstmicroservice.controller;
+
+import com.example.firstmicroservice.model.Reservation;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/reservation")
+public class ReservationController {
+
+    @GetMapping
+    public List<Reservation> getStudent() {
+        System.out.println("getting reservations");
+        return Arrays.asList(new Reservation(1,"Miso","123-YYY"),
+        new Reservation(2,"Miso","123-XXXX"));
+    }
+
+    @PostMapping
+    public Boolean createStudent(@RequestBody Reservation reservation) {
+        System.out.println("creating reservation:"+ reservation);
+        return true;
+    }
+
+}
