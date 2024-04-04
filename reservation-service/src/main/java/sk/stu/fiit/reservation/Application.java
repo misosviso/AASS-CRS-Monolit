9e796a0261d368ba5424726a86605524098b797c
misosviso <michal.komora@protonmail.com>
2024-04-04 17:46:26 +0200

    Camunda external service

--- reservation-service/src/main/java/sk/stu/fiit/reservation/Application.java
+++ reservation-service/src/main/java/sk/stu/fiit/reservation/Application.java
@@ -1,13 +1,46 @@
 package sk.stu.fiit.reservation;
 
 import org.springframework.boot.SpringApplication;
+import org.camunda.bpm.client.ExternalTaskClient;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.awt.*;
+import java.net.URI;
+import java.util.logging.Logger;
+
 
 @SpringBootApplication
 public class Application {
+	private final static Logger LOGGER = Logger.getLogger(Application.class.getName());
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ExternalTaskClient client = ExternalTaskClient.create()
+				.baseUrl("http://localhost:8080/engine-rest")
+				.asyncResponseTimeout(10000) // long polling timeout
+				.build();
+
+		// subscribe to an external task topic as specified in the process
+		client.subscribe("write-reservation")
+				.lockDuration(1000) // the default lock duration is 20 seconds, but you can override this
+				.handler((externalTask, externalTaskService) -> {
+					// Put your business logic here
+
+//					// Get a process variable
+//					String item = externalTask.getVariable("item");
+//					Integer amount = externalTask.getVariable("amount");
+
+					LOGGER.info("Writing reservation");
+
+//					try {
+//						Desktop.getDesktop().browse(new URI("https://docs.camunda.org/get-started/quick-start/complete"));
+//					} catch (Exception e) {
+//						e.printStackTrace();
+//					}
+//
+//					// Complete the task
+					externalTaskService.complete(externalTask);
+				})
+				.open();
 	}
 
 }
--- reservation-service/src/main/java/sk/stu/fiit/reservation/controller/ReservationController.java
+++ reservation-service/src/main/java/sk/stu/fiit/reservation/controller/ReservationController.java
@@ -1,41 +1,41 @@
-package sk.stu.fiit.reservation.controller;
-
-import org.apache.http.HttpException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.server.reactive.ServerHttpResponse;
-import org.springframework.web.server.ResponseStatusException;
-import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Mono;
-import sk.stu.fiit.reservation.model.Reservation;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
-
-@RestController
-@RequestMapping("/reservation")
-public class ReservationController {
-
-    private final List<Reservation> reservations = new ArrayList<>();
-
-    @GetMapping
-    public List<Reservation> getStudent(@RequestHeader(value="id") String id, @RequestHeader(value="role") String role) {
-        if (Objects.equals(role, "doctor")) {
-            System.out.println("getting reservations");
-            return reservations;
-        }
-        throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Unauthorized");
-    }
-
-    @PostMapping
-    public Boolean createStudent(@RequestBody Reservation reservation, @RequestHeader(value="role") String role) {
-        if (Objects.equals(role, "patient")) {
-            System.out.println("getting reservations");
-            reservations.add(reservation);
-        }
-        return true;
-    }
-
-}
+//package sk.stu.fiit.reservation.controller;
+//
+//import org.apache.http.HttpException;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.server.reactive.ServerHttpResponse;
+//import org.springframework.web.server.ResponseStatusException;
+//import org.springframework.web.server.ServerWebExchange;
+//import reactor.core.publisher.Mono;
+//import sk.stu.fiit.reservation.model.Reservation;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.List;
+//import java.util.Objects;
+//
+//@RestController
+//@RequestMapping("/reservation")
+//public class ReservationController {
+//
+//    private final List<Reservation> reservations = new ArrayList<>();
+//
+//    @GetMapping
+//    public List<Reservation> getStudent(@RequestHeader(value="id") String id, @RequestHeader(value="role") String role) {
+//        if (Objects.equals(role, "doctor")) {
+//            System.out.println("getting reservations");
+//            return reservations;
+//        }
+//        throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Unauthorized");
+//    }
+//
+//    @PostMapping
+//    public Boolean createStudent(@RequestBody Reservation reservation, @RequestHeader(value="role") String role) {
+//        if (Objects.equals(role, "patient")) {
+//            System.out.println("getting reservations");
+//            reservations.add(reservation);
+//        }
+//        return true;
+//    }
+//
+//}
