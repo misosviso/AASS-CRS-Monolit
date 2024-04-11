f1c527026a7420e07eeb1d1a08e087625f55c548
misosviso <michal.komora@protonmail.com>
2024-04-11 17:35:11 +0200

    Component scan

--- reservation-service/src/main/java/sk/stu/fiit/reservation/listeners/CustomExecutionListener.java
+++ reservation-service/src/main/java/sk/stu/fiit/reservation/listeners/CustomExecutionListener.java
@@ -2,7 +2,9 @@ package sk.stu.fiit.reservation.listeners;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.ExecutionListener;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CustomExecutionListener implements ExecutionListener {
     @Override
     public void notify(DelegateExecution delegateExecution) throws Exception {
